c2d6d2cc2e8d36d506f8e16a8c1b7240210c2663
Mohamed864 <beherym884@gmail.com>
2024-05-30 18:00:16 +0200

    4rth update

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,28 +11,28 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                   {
-                    title: 'hats',
+                    title: 'HATS',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1
                   },
                   {
-                    title: 'jackets',
+                    title: 'JACKETS',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2
                   },
                   {
-                    title: 'sneakers',
+                    title: 'SNEAKERS',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3
                   },
                   {
-                    title: 'womens',
+                    title: 'WOMEN',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4
                   },
                   {
-                    title: 'mens',
+                    title: 'MEN',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import './menu-item.style.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
 
-        backgroundImage: `url(${imageUrl})`
+            style={{
 
-    }} className={`${size} menu-item`}>
+            backgroundImage: `url(${imageUrl})`
+
+}}
+
+        />
         <div className='content'>
-            <h1 className='title'>{ title }</h1>
+            <h1 className='title'>{ title.toUpperCase() }</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
