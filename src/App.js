fd12504dfee4484dbcd70020a386017dfdb870b5
Mohamed864 <beherym884@gmail.com>
2024-05-31 16:54:47 +0200

    adding header comonent

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import HomePage from './pages/homepage/homepage.component';
 
 import ShopPage from './pages/shop/shop.component.jsx';
 
+import Header from './components/header/header.component';
+
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} /> 
         <Route path='/shop' element={<ShopPage />} /> 
