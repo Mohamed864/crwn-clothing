ae2db36af342434a3417ac9fd748d130cc81817b
Mohamed864 <beherm884@gmail.com>
2024-12-09 16:55:34 +0200

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HAT PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 //there is a new way to right routes in react course
 function App() {
@@ -16,7 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage />} />
-          <Route path="/shop/hats" element={<HatsPage />} />
+          <Route path="/shop" element={<ShopPage />} />
         </Routes>
       </Router>
     </div>
