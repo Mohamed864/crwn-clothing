8d85d4fc469d8e8d2b94fc9e0096c6cde757c12b
Mohamed864 <beherm884@gmail.com>
2024-12-09 16:03:44 +0200

    router done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HAT PAGE</h1>
+  </div>
+);
+
+//there is a new way to right routes in react course
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/shop/hats" element={<HatsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom"; // takes component as an argument and return the modified comp
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate(); //equavilent to history push
+  const location = useLocation(); //equavilent to match url
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => navigate(`${location.pathname}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{
