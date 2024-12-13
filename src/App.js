f3b66815ceb4fddc92d8be6f15b53f6cde3fb81f
Mohamed864 <beherm884@gmail.com>
2024-12-13 09:06:40 +0200

    signinandsignup done

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ class App extends React.Component {
               console.log(this.state);
             }*/
           );
-          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: null });
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.commponent";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -14,9 +15,21 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const { user } = userCredential;
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (e) => {
